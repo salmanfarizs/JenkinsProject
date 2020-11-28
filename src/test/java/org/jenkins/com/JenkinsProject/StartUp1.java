a5d1ffbc6e46cdc92bce289f20ed4a9572eb348d
Mohammed Salman <smohammedfariz@gmail.com>
2020-11-28 14:16:54 +0530

    manifest,build run sample success

---
+++ src/test/java/org/jenkins/com/JenkinsProject/StartUp1.java
@@ -0,0 +1,21 @@
+package org.jenkins.com.JenkinsProject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class StartUp1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver;
+		
+		System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver.exe");
+		// Initialize browser.
+		 driver=new ChromeDriver();
+		 System.out.println("running");
+		 driver.get("https://accounts.google.com/");
+
+	}
+
+}
